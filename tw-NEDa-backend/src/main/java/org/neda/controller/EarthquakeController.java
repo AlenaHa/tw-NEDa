adb1ada08d61a69c2b7b1f8fe0e7144b3bac7d76
AlenaHa <andreea.hardon@gmail.com>
2017-04-11 21:43:47 +0300

    Added CRUD methods in the controller #11.

--- tw-NEDa-backend/src/main/java/org/neda/controller/EarthquakeController.java
+++ tw-NEDa-backend/src/main/java/org/neda/controller/EarthquakeController.java
@@ -4,9 +4,9 @@ package org.neda.controller;
 import org.neda.entity.Earthquake;
 import org.neda.service.EarthquakeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -15,9 +15,41 @@ public class EarthquakeController {
     @Autowired
     private EarthquakeService earthquakeService;
 
-    @RequestMapping(value = "/earthquake", method = RequestMethod.GET)
-    public Earthquake earthquakeList() {
-        return earthquakeService.findById(5l);
+    /**
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/earthquake/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Earthquake> readEarthquake(@PathVariable Long id) {
+        Earthquake earthquake = earthquakeService.findById(id);
+        if (earthquake != null) {
+            return new ResponseEntity<Earthquake>(earthquake, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<Earthquake>(HttpStatus.NOT_FOUND);
+        }
+
     }
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/earthquake/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Earthquake> deleteEarthquake(@PathVariable Long id) {
+        earthquakeService.delete(id);
+        return new ResponseEntity<Earthquake>(HttpStatus.ACCEPTED);
+    }
+
+    /**
+     * @param earthquake
+     * @return
+     */
+    @RequestMapping(value = "/earthquake", method = RequestMethod.POST)
+    public ResponseEntity<Earthquake> createEarthquake(@RequestBody Earthquake earthquake) {
+        Earthquake savedEarthquake = earthquakeService.save(earthquake);
+        return new ResponseEntity<Earthquake>(savedEarthquake, HttpStatus.CREATED);
+    }
+
 
 }
