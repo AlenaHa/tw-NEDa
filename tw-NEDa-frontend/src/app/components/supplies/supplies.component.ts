38bfd0aed39a8012d80ceecee027b89de6273cd1
AlenaHa <andreea.hardon@gmail.com>
2017-04-25 15:09:06 +0300

    - improved supplies component - added some ideas regarding what people would like to know about this subject & what it would help them with

--- tw-NEDa-frontend/src/app/components/supplies/supplies.component.ts
+++ tw-NEDa-frontend/src/app/components/supplies/supplies.component.ts
@@ -6,4 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./supplies.component.scss']
 })
 
-export class SuppliesComponent{}
+export class SuppliesComponent{
+  selectedValue: string;
+
+  locations = [
+    {value: 'location-0', viewValue: 'Kathmandu'},
+    {value: 'location-1', viewValue: 'Bhaktapur'},
+    {value: 'location-2', viewValue: 'Pyuthan'}
+  ];
+
+  supplies = [
+    {value: 'supply-0', viewValue: 'Food'},
+    {value: 'supply-1', viewValue: 'Wather'},
+    {value: 'supply-2', viewValue: 'Clothes'}
+  ];
+
+  suppliesType = [
+    {value: 'sType-0', viewValue: 'Shelter'},
+    {value: 'sType-1', viewValue: 'Food'},
+    {value: 'sType-2', viewValue: 'Medicine'}
+  ];
+}
